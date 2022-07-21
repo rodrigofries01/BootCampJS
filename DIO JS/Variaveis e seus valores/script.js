1fd76e0cffd8f93914d9edfd290db33502dff6fe
rodrigofries01 <rfries24@gmail.com>
2022-07-21 13:29:14 -0300

    commit

---
+++ DIO JS/Variaveis e seus valores/script.js
@@ -0,0 +1,19 @@
+//tipos primitivos 
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Rodrigo';
+console.log(typeof('nome'));
+
+//function
+var funcao = function(){
+  console.log(typeof(funcao));
+}
+
